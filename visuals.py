f41b10523b7260ec0ca60ea036cc0ed636cda950
Brayden Boyko <braydenboyko@boykowealth.com>
2025-02-27 13:45:32 -0700

    Added Alpha Graphs

--- visuals.py
+++ visuals.py
@@ -35,11 +35,9 @@ def monthly_ivol():
     plt.show()
 
 def monthly_total_return():
-
     portfolio = portfolio_returns()
     portfolio['CUM_RET'] = portfolio.groupby(['PORTFOLIO'])['RET'].transform(lambda x: np.cumprod(1 + x))
 
-    portfolio.to_csv(r"c:\Users\brayd\Downloads\test.csv")
     plt.figure(figsize=(12, 6))
     for port in portfolio['PORTFOLIO'].unique():
         data = portfolio[portfolio['PORTFOLIO'] == port]
@@ -48,6 +46,28 @@ def monthly_total_return():
     plt.xlabel("Date")
     plt.ylabel("Return")
     plt.title("Monthly Portfolio Returns Over Time")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+def monthly_alpha():
+    portfolio = portfolio_returns()
+    portfolio = portfolio[['DATE', 'PORTFOLIO', 'RET']]
+    portfolio = portfolio.pivot(index='DATE', columns='PORTFOLIO', values='RET')
+
+    portfolio['ALPHA_L'] = portfolio['LongPosV2'] - portfolio['LongPosV1']
+    portfolio['ALPHA_S'] = portfolio['ShortPosV2'] - portfolio['ShortPosV1']
+
+    portfolio['ALPHA_L'] = (1 + portfolio['ALPHA_L']).cumprod()
+    portfolio['ALPHA_S'] = (1 + portfolio['ALPHA_S']).cumprod()
+    
+    plt.figure(figsize=(12, 6))
+    plt.plot(portfolio.index, portfolio['ALPHA_L'], label='Alpha Long')
+    plt.plot(portfolio.index, portfolio['ALPHA_S'], label='Alpha Short')
+
+    plt.xlabel("Date")
+    plt.ylabel("Alpha")
+    plt.title("Monthly Portfolio Alpha Over Time")
     plt.legend()
     plt.grid(True)
     plt.show()
